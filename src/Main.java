4504f84d5244d5c3be3dd3e30509c32dad722f86
BBg1007 <bbgalexson@gmail.com>
2024-12-02 20:50:08 +0300

    Homework is done.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,65 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main (String[] args) {
+    Author author1 = new Author("Joanne", "Rowling");
+    Author author2 = new Author("Andrzej","Sapkowski");
+    Book harryPotter = new Book("Harry Potter",author1,1997);
+    Book theWitcher = new Book("The Witcher",author2,1986);
+    System.out.println();
+    System.out.println("Книга "+harryPotter.getBookName()+" написана "+harryPotter.getAuthor()+" в "+harryPotter.getPublishingYear()+".");
+    System.out.println();
+    System.out.println("Книга "+theWitcher.getBookName()+" написана "+theWitcher.getAuthor()+" в "+theWitcher.getPublishingYear()+".");
+    System.out.println();
+    theWitcher.setPublishingYear(1988);
+    System.out.println("Книга "+theWitcher.getBookName()+" переизданна в "+theWitcher.getPublishingYear()+".");
+
+
+
     }
-}
+        public static class Author {
+            private final String authorName;
+            private final String authorSName;
+
+            public Author(String authorName, String authorSName) {
+                this.authorName = authorName;
+                this.authorSName = authorSName;
+            }
+
+            public String getAuthorName() {
+                return authorName;
+            }
+
+            public String getAuthorSName() {
+                return authorSName;
+            }
+
+        }
+
+        public static class Book {
+            private final String bookName;
+            private final Author author;
+            private int publishingYear;
+
+            public Book(String bookName, Author author, int publishingYear) {
+                this.bookName = bookName;
+                this.author = author;
+                this.publishingYear = publishingYear;
+            }
+
+            public String getBookName() {
+                return bookName;
+            }
+
+            public int getPublishingYear() {
+                return publishingYear;
+            }
+
+            public String getAuthor() {
+                return author.authorName+" "+author.authorSName;
+            }
+
+            public void setPublishingYear(int publishingYear) {
+                this.publishingYear = publishingYear;
+            }
+        }
+
+    }
